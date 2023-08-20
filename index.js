880b73c097516d111a890449279df23cc8519b3b
je-delray <hovax74935@ekcsoft.com>
2023-08-20 16:00:51 +0200

    bugfix

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 function generateList(array) {
-    const res = array.map(item =>
-        typeof item === "number" ? `<li>${item}</li>` : `<ul>${generateList(item)}</ul>`
-    )
+    let counter = 1
+    const res = array.map((item, index) => {
+        if (Array.isArray(item)) {
+            return `<ul>${generateList(item)}</ul>`
+        } else {
+            if (item !== array[index - 1]) {
+                return `<li>${item}</li>`
+            } else {
+                return `<li>${item}.${counter++}</li>`
+            }
+        }
+    })
     return res.join("")
 }
 
-const arr = [1, 2, 3, [1, 2, 3, 4, [1, 2]]]
+const arr = [1, 2, 3, [1, 2, 3, 4, [1, 1, 1, 1]]]
 const resultHTML = generateList(arr)
 
 document.body.innerHTML += resultHTML
