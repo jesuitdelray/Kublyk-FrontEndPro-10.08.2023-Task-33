3fecf81eeaeaad714b4b08d8a7c19227a5f37459
je-delray <hovax74935@ekcsoft.com>
2023-08-20 22:38:02 +0200

    bugfix 2.0

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-function generateList(array) {
-    let counter = 1
-    const res = array.map((item, index) => {
-        if (Array.isArray(item)) {
-            return `<ul>${generateList(item)}</ul>`
-        } else {
-            if (item !== array[index - 1]) {
-                return `<li>${item}</li>`
+function generateList(array, prevItem = null, counter = 1) {
+    return array
+        .map(item => {
+            if (Array.isArray(item)) {
+                return `<ul>${generateList(item)}</ul>`
             } else {
-                return `<li>${item}.${counter++}</li>`
+                if (item !== prevItem) {
+                    counter = 0
+                    prevItem = item
+                } else {
+                    counter++
+                }
+                return `<li>${item}${counter > 0 ? "." + counter : ""}</li>`
             }
-        }
-    })
-    return res.join("")
+        })
+        .join("")
 }
 
-const arr = [1, 2, 3, [1, 2, 3, 4, [1, 1, 1, 1]]]
+const arr = [1, 2, 3, [1, 2, 3, 4, [1, 1.2, 1.2, 1.2]]]
 const resultHTML = generateList(arr)
 
 document.body.innerHTML += resultHTML
